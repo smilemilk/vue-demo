7b2a59a44c4f8d11e14c3bb9fe14c53dc388018b
smilemilk <936360925@qq.com>
2018-07-05 15:36:05 +0800

    m

--- config/changeServer.js
+++ config/changeServer.js
@@ -6,11 +6,12 @@ var argument = process.argv.splice(2)
 var server = ''
 
 if (argument[0] === 'build') { // 测试
-  server = 'http://test.weimaipay.com:8070/'
-} else if (argument[0] === 'yfb') { // 预发布
-  server = 'http://www.weimaipay.com:8070/'
-} else if (argument[0] === 'production') { // 生产（线上）
-  server = 'http://www.weimaipay.com:8070/'
+  server = 'http://test.weimaipay.com:8070/'  // 微脉 地址通过替换由后端执行
+// }
+// else if (argument[0] === 'yfb') { // 预发布
+//   server = 'http://www.weimaipay.com:8070/'
+// } else if (argument[0] === 'production') { // 生产（线上）
+//   server = 'http://www.weimaipay.com:8070/'
 } else if (argument[0] === 'development') { // 开发
   server = ''
 }
--- config/server.js
+++ config/server.js
@@ -1,1 +1,1 @@
-module.exports = ""
+module.exports = "http://test.weimaipay.com:8070/"
